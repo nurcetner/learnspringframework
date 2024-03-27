bcd32617b584206bd865cb87dbd274751b643fc8
nurcetner <137170163+nurcetner@users.noreply.github.com>
2024-03-27 09:54:10 +0300

    Add files via upload
    ilk commit

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learnspringframework.game;
+
+public class MarioGame implements GamingConsole {
+
+	
+	 public void up() {
+	   System.out.println("jump");
+	 }
+	 
+	 public void down() {
+		 System.out.println("go into a hole");
+	 }
+	 
+	 public void left() {
+		 System.out.println("go back");
+	 }
+	 
+	 public void right() {
+		 System.out.println("Accelarate");
+	 }
+	 
+	 
+	 
+}
---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/SuperContraGame.java
@@ -0,0 +1,23 @@
+package com.in28minutes.learnspringframework.game;
+
+public class SuperContraGame implements GamingConsole {
+	
+
+	 public void up() {
+	   System.out.println("up");
+	 }
+	 
+	 public void down() {
+		 System.out.println("sit down");
+	 }
+	 
+	 public void left() {
+		 System.out.println("go back");
+	 }
+	 
+	 public void right() {
+		 System.out.println("shoot a bullet");
+	 }
+	 
+
+}
